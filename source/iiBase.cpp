43023ddb010fe2edf63aee2de23311ec63345bcd
Michail Denchev <mdenchev@gmail.com>
2012-03-10 23:02:23 -0500

    Added actions to fileMenu: openFile savefile exit Only saveFile is hooked to a slot, but still WIP.
    Editor starts maximized now.

--- source/iiBase.cpp
+++ source/iiBase.cpp
@@ -4,6 +4,34 @@
 iiBase::iiBase()
   : QMainWindow()
 {
+  setWindowState(Qt::WindowMaximized);
+
   setCentralWidget(&mainText);
+
+  // File actions
+  openFileAction = new QAction(tr("Open File"), this);
+  saveFileAction = new QAction(tr("Save File"), this);
+  connect(saveFileAction, SIGNAL(triggered()), this, SLOT(showSaveFileDialog()));
+
+  // Program actions
+  exitProgramAction = new QAction(tr("Exit"), this);
+
+  // Create and populate fileMenu
+  fileMenu = menuBar()->addMenu(tr("File"));
+  fileMenu->addAction(openFileAction);
+  fileMenu->addAction(saveFileAction);
+  fileMenu->addAction(exitProgramAction);
+
 }
 
+QSize iiBase::sizeHint() const
+{
+  return QSize(500,500);
+  //return QSize(QApplication::desktop()->availableGeometry().size());
+}
+
+void iiBase::showSaveFileDialog()
+{
+  QDialog saveFileDialog(this);
+  saveFileDialog.exec();
+}
--- source/iiBase.h
+++ source/iiBase.h
@@ -4,6 +4,11 @@
 #include <QApplication>
 #include <QMainWindow>
 #include <QTextEdit>
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+#include <QDialog>
+#include <QDesktopWidget>
 
 class iiBase : public QMainWindow
 {
@@ -12,8 +17,28 @@ class iiBase : public QMainWindow
   public:
     iiBase();
 
+    QSize sizeHint() const;
+
+  signals:
+    void saveFile();
+
+  public slots:
+    void showSaveFileDialog();
+
   private:
     QTextEdit mainText;
+
+    // menus
+    QMenu *fileMenu;
+    QMenu *helpMenu;
+
+    // menu actions
+    QAction *openFileAction;
+    QAction *saveFileAction;
+
+
+    // program actions
+    QAction *exitProgramAction;
 };
 
 #endif 
