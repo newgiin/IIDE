5bf3355a8bbb52a54ce046c409dc12a909c64a40
Michail Denchev <mdenchev@gmail.com>
2012-06-14 13:13:13 -0400

    Add forgotten iiPythonHighlighter.h

---
+++ source/iiPythonHighlighter.h
@@ -0,0 +1,30 @@
+#ifndef II_PYTHON_HIGHLIGHTER
+#define II_PYTHON_HIGHLIGHTER
+
+#include <QSyntaxHighlighter>
+
+class iiPythonHighlighter : public QSyntaxHighlighter
+{
+  Q_OBJECT
+
+  public:
+    iiPythonHighlighter(QTextDocument *parent = 0);
+
+  protected:
+    void highlightBlock(const QString &text);
+
+  private:
+    struct HighlightingRule
+    {
+      QRegExp pattern;
+      QTextCharFormat format;
+    };
+    QVector<HighlightingRule> highlightingRules;
+
+    QTextCharFormat keywordFormat;
+    QTextCharFormat commentFormat;
+    QTextCharFormat quotationFormat;
+};
+
+
+#endif
